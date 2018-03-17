902458c312b38865ebd7f9c865cb13aba60ed68c
darrena092 <darrena092@gmail.com>
2018-03-17 08:39:14 +0000

    Fixed Linux module build and shared library loading.

--- src/cpu-module-kvm/cpu_kvm.cpp
+++ src/cpu-module-kvm/cpu_kvm.cpp
@@ -2,12 +2,87 @@
 #include "cpu_kvm.h"
 #include "openxbox/log.h"
 
+#include <fcntl.h>
+
 namespace openxbox {
 
 KvmCpu::KvmCpu() {
+    m_kvm = nullptr;
 }
 
 KvmCpu::~KvmCpu() {
+    if(m_kvm != nullptr) {
+        delete m_kvm;
+        m_kvm = nullptr;
+    }
+}
+
+int KvmCpu::InitializeImpl() {
+    if(m_kvm == nullptr) {
+        m_kvm = new Kvm;
+    }
+
+    return 0;
+}
+
+int KvmCpu::RunImpl() {
+    return 0;
+}
+
+int KvmCpu::StepImpl(uint64_t num_instructions) {
+    return 0;
+}
+
+InterruptResult KvmCpu::InterruptImpl(uint8_t vector) {
+    return *(new InterruptResult);
+}
+
+int KvmCpu::MemMapSubregion(MemoryRegion *subregion) {
+    return 0;
+}
+
+int KvmCpu::MemRead(uint32_t addr, uint32_t size, void *value) {
+    return 0;
+}
+
+int KvmCpu::MemWrite(uint32_t addr, uint32_t size, void *value) {
+    return 0;
+}
+
+int KvmCpu::RegRead(enum CpuReg reg, uint32_t *value) {
+    return 0;
+}
+
+int KvmCpu::RegWrite(enum CpuReg reg, uint32_t value) {
+    return 0;
+}
+
+int KvmCpu::GetGDT(uint32_t *addr, uint32_t *size) {
+    return 0;
+}
+
+int KvmCpu::SetGDT(uint32_t addr, uint32_t size) {
+    return 0;
+}
+
+int KvmCpu::GetIDT(uint32_t *addr, uint32_t *size) {
+    return 0;
+}
+
+int KvmCpu::SetIDT(uint32_t addr, uint32_t size) {
+    return 0;
+}
+
+int KvmCpu::ReadMSR(uint32_t reg, uint64_t *value) {
+    return 0;
+}
+
+int KvmCpu::WriteMSR(uint32_t reg, uint64_t value) {
+    return 0;
+}
+
+int KvmCpu::InvalidateTLBEntry(uint32_t addr) {
+    return 0;
 }
 
 }
--- src/cpu-module-kvm/cpu_kvm.h
+++ src/cpu-module-kvm/cpu_kvm.h
@@ -2,7 +2,33 @@
 
 #include "openxbox/cpu.h"
 
+#include <linux/kvm.h>
+#include <vector>
+
 namespace openxbox {
+
+typedef struct {
+    uint32_t startingAddress;
+    uint32_t size;
+    struct kvm_userspace_memory_region memoryRegion;
+} PhysicalMemoryRange;
+
+typedef struct {
+    int dev_id;
+    int dev_fd;
+    struct kvm_run* kvm_run;
+    int kvm_run_mmap_size;
+    struct kvm_regs regs;
+    struct kvm_sregs sregs;
+    struct kvm_guest_debug debug;
+} KvmVCpu;
+
+typedef struct {
+    int dev_fd;
+    int vm_fd;
+    std::vector<PhysicalMemoryRange*> physicalMemoryRanges;
+    KvmVCpu vCpu;
+} Kvm;
 
 class KvmCpu : public Cpu {
 
@@ -10,6 +36,33 @@ public:
     KvmCpu();
     ~KvmCpu();
 
+    int InitializeImpl();
+
+    int RunImpl();
+    int StepImpl(uint64_t num_instructions);
+    InterruptResult InterruptImpl(uint8_t vector);
+
+    int MemMapSubregion(MemoryRegion *subregion);
+    int MemRead(uint32_t addr, uint32_t size, void *value);
+    int MemWrite(uint32_t addr, uint32_t size, void *value);
+
+    int RegRead(enum CpuReg reg, uint32_t *value);
+    int RegWrite(enum CpuReg reg, uint32_t value);
+
+    int GetGDT(uint32_t *addr, uint32_t *size);
+    int SetGDT(uint32_t addr, uint32_t size);
+
+    int GetIDT(uint32_t *addr, uint32_t *size);
+    int SetIDT(uint32_t addr, uint32_t size);
+
+    int ReadMSR(uint32_t reg, uint64_t *value);
+    int WriteMSR(uint32_t reg, uint64_t value);
+
+    int InvalidateTLBEntry(uint32_t addr);
+
+
+private:
+    Kvm *m_kvm;
 };
 
 }
