a8592addc23d18792dc81471680c1729ec937814
StrikerX3 <ivan.rober@gmail.com>
2018-03-18 12:28:34 -0300

    Null check IRQs

--- src/core/openxbox/hw/basic/gsi.cpp
+++ src/core/openxbox/hw/basic/gsi.cpp
@@ -4,9 +4,13 @@ namespace openxbox {
 
 void GSI::HandleIRQ(uint8_t irqNum, int level) {
     if (irqNum < ISA_NUM_IRQS) {
-        i8259IRQs[irqNum]->Handle(level);
+        if (i8259IRQs[irqNum] != nullptr) {
+            i8259IRQs[irqNum]->Handle(level);
+        }
     }
-    ioapicIRQs[irqNum]->Handle(level);
+    if (ioapicIRQs[irqNum] != nullptr) {
+        ioapicIRQs[irqNum]->Handle(level);
+    }
 }
 
 }
