db01ec02639a78f9d9527a0931f82c8af3944cb7
StrikerX3 <ivan.rober@gmail.com>
2018-12-03 16:27:17 -0200

    Begin structuring the ATA emulator

--- src/core/openxbox/hw/basic/ata.cpp
+++ src/core/openxbox/hw/basic/ata.cpp
@@ -4,10 +4,11 @@
 #include "openxbox/io.h"
 
 namespace openxbox {
+namespace hw {
+namespace ata {
 
 ATA::ATA(i8259 *pic)
-    : m_pic(pic)
-{
+    : m_pic(pic) {
 }
 
 ATA::~ATA() {
@@ -27,16 +28,152 @@ bool ATA::MapIO(IOMapper *mapper) {
 }
 
 bool ATA::IORead(uint32_t port, uint32_t *value, uint8_t size) {
+    if (port >= PORT_PRIMARY_ATA_COMMAND_BASE && port <= PORT_PRIMARY_ATA_COMMAND_LAST) {
+        return ReadCommandPort((Register) (port - PORT_PRIMARY_ATA_COMMAND_BASE), ChanPrimary, value, size);
+    }
+    if (port == PORT_PRIMARY_ATA_CONTROL) {
+        return ReadControlPort(ChanPrimary, value, size);
+    }
+    if (port >= PORT_SECONDARY_ATA_COMMAND_BASE && port <= PORT_SECONDARY_ATA_COMMAND_LAST) {
+        return ReadCommandPort((Register)(port - PORT_SECONDARY_ATA_COMMAND_BASE), ChanSecondary, value, size);
+    }
+    if (port == PORT_SECONDARY_ATA_CONTROL) {
+        return ReadControlPort(ChanSecondary, value, size);
+    }
+
     log_warning("ATA::IORead:  Unhandled read!   port = 0x%x,  size = %u\n", port, size);
-
     *value = 0;
-
     return false;
 }
 
 bool ATA::IOWrite(uint32_t port, uint32_t value, uint8_t size) {
+    if (port >= PORT_PRIMARY_ATA_COMMAND_BASE && port <= PORT_PRIMARY_ATA_COMMAND_LAST) {
+        return WriteCommandPort((Register)(port - PORT_PRIMARY_ATA_COMMAND_BASE), ChanPrimary, value, size);
+    }
+    if (port == PORT_PRIMARY_ATA_CONTROL) {
+        return WriteControlPort(ChanPrimary, value, size);
+    }
+    if (port >= PORT_SECONDARY_ATA_COMMAND_BASE && port <= PORT_SECONDARY_ATA_COMMAND_LAST) {
+        return WriteCommandPort((Register)(port - PORT_SECONDARY_ATA_COMMAND_BASE), ChanSecondary, value, size);
+    }
+    if (port == PORT_SECONDARY_ATA_CONTROL) {
+        return WriteControlPort(ChanSecondary, value, size);
+    }
+
     log_warning("ATA::IOWrite: Unhandled write!  port = 0x%x,  size = %u,  value = 0x%x\n", port, size, value);
     return false;
 }
 
+bool ATA::ReadCommandPort(Register reg, Channel channel, uint32_t *value, uint8_t size) {
+    switch (reg) {
+    case RegData:
+        return ReadData(channel, value, size);
+    case RegError:
+        return ReadError(channel, value, size);
+    case RegSectorCount:
+        if (size != 1) { log_spew("ATA::ReadCommandPort: Unexpected read of size %d from register %d, channel %d\n", size, reg, channel); }
+        *value = m_reg_sectorCount;
+        return true;
+    case RegSectorNumber:
+        if (size != 1) { log_spew("ATA::ReadCommandPort: Unexpected read of size %d from register %d, channel %d\n", size, reg, channel); }
+        *value = m_reg_sectorNumber;
+        return true;
+    case RegCylinderLow:
+        if (size != 1) { log_spew("ATA::ReadCommandPort: Unexpected read of size %d from register %d, channel %d\n", size, reg, channel); }
+        *value = m_reg_cylinder & 0xFF;
+        return true;
+    case RegCylinderHigh:
+        if (size != 1) { log_spew("ATA::ReadCommandPort: Unexpected read of size %d from register %d, channel %d\n", size, reg, channel); }
+        *value = (m_reg_cylinder >> 8) & 0xFF;
+        return true;
+    case RegDeviceHead:
+        if (size != 1) { log_spew("ATA::ReadCommandPort: Unexpected read of size %d from register %d, channel %d\n", size, reg, channel); }
+        *value = m_reg_deviceHead;
+        return true;
+    case RegStatus:
+        return ReadStatus(channel, value, size);
+    default:
+        log_warning("ATA::ReadCommandPort:  Invalid register %d  (channel = %d  size = %d)\n", reg, channel, size);
+        *value = 0;
+        return false;
+    }
+}
+
+bool ATA::WriteCommandPort(Register reg, Channel channel, uint32_t value, uint8_t size) {
+    switch (reg) {
+    case RegData:
+        return WriteData(channel, value, size);
+    case RegFeatures:
+        if (size != 1) { log_spew("ATA::WriteCommandPort: Unexpected write of size %d from register %d, channel %d  (value = 0x%x)\n", size, reg, channel, value); }
+        m_reg_features = value;
+        return true;
+    case RegSectorCount:
+        if (size != 1) { log_spew("ATA::WriteCommandPort: Unexpected write of size %d from register %d, channel %d  (value = 0x%x)\n", size, reg, channel, value); }
+        m_reg_sectorCount = value;
+        return true;
+    case RegSectorNumber:
+        if (size != 1) { log_spew("ATA::WriteCommandPort: Unexpected write of size %d from register %d, channel %d  (value = 0x%x)\n", size, reg, channel, value); }
+        m_reg_sectorNumber = value;
+        return true;
+    case RegCylinderLow:
+        if (size != 1) { log_spew("ATA::WriteCommandPort: Unexpected write of size %d from register %d, channel %d  (value = 0x%x)\n", size, reg, channel, value); }
+        m_reg_cylinder = (m_reg_cylinder & 0xFF00) | (value & 0xFF);
+        return true;
+    case RegCylinderHigh:
+        if (size != 1) { log_spew("ATA::WriteCommandPort: Unexpected write of size %d from register %d, channel %d  (value = 0x%x)\n", size, reg, channel, value); }
+        m_reg_cylinder = (m_reg_cylinder & 0x00FF) | ((value & 0xFF) << 8);
+        return true;
+    case RegDeviceHead:
+        if (size != 1) { log_spew("ATA::WriteCommandPort: Unexpected write of size %d from register %d, channel %d  (value = 0x%x)\n", size, reg, channel, value); }
+        m_reg_deviceHead = value;
+        return true;
+    case RegCommand:
+        return WriteCommand(channel, value, size);
+    default:
+        log_warning("ATA::WriteCommandPort:  Invalid register %d  (channel = %d  size = %d  value = 0x%x)\n", reg, channel, size, value);
+        return false;
+    }
+}
+
+bool ATA::ReadControlPort(Channel channel, uint32_t *value, uint8_t size) {
+    log_warning("ATA::ReadControlPort:   Unimplemented!  (channel = %d  size = %d)\n", channel, size);
+    *value = 0;
+    return false;
+}
+
+bool ATA::WriteControlPort(Channel channel, uint32_t value, uint8_t size) {
+    log_warning("ATA::WriteControlPort:  Unimplemented!  (channel = %d  size = %d  value = 0x%x)\n", channel, size, value);
+    return false;
+}
+
+bool ATA::ReadData(Channel channel, uint32_t *value, uint8_t size) {
+    log_warning("ATA::ReadData:  Unimplemented!  (channel = %d  size = %d)\n", channel, size);
+    *value = 0;
+    return false;
+}
+
+bool ATA::ReadError(Channel channel, uint32_t *value, uint8_t size) {
+    log_warning("ATA::ReadError:  Unimplemented!  (channel = %d  size = %d)\n", channel, size);
+    *value = 0;
+    return false;
+}
+
+bool ATA::ReadStatus(Channel channel, uint32_t *value, uint8_t size) {
+    log_warning("ATA::ReadStatus:  Unimplemented!  (channel = %d  size = %d)\n", channel, size);
+    *value = 0;
+    return false;
+}
+
+bool ATA::WriteData(Channel channel, uint32_t value, uint8_t size) {
+    log_warning("ATA::WriteData:  Unimplemented!  (channel = %d  size = %d  value = 0x%x)\n", channel, size, value);
+    return false;
+}
+
+bool ATA::WriteCommand(Channel channel, uint32_t value, uint8_t size) {
+    log_warning("ATA::WriteCommand:  Unimplemented!  (channel = %d  size = %d  value = 0x%x)\n", channel, size, value);
+    return false;
+}
+
+}
+}
 }
--- src/core/openxbox/hw/basic/ata.h
+++ src/core/openxbox/hw/basic/ata.h
@@ -5,18 +5,48 @@
 #include "openxbox/cpu.h"
 #include "../basic/i8259.h"
 
+
+// ATA/ATAPI-4 specification:
+// http://www.t13.org/documents/UploadedDocuments/project/d1153r18-ATA-ATAPI-4.pdf
+
 namespace openxbox {
+namespace hw {
+namespace ata {
 
 #define PRIMARY_ATA_IRQ                   14
 #define PORT_PRIMARY_ATA_COMMAND_BASE     0x1f0
 #define PORT_PRIMARY_ATA_COMMAND_COUNT    8
+#define PORT_PRIMARY_ATA_COMMAND_LAST     (PORT_PRIMARY_ATA_COMMAND_BASE + PORT_PRIMARY_ATA_COMMAND_COUNT - 1)
 #define PORT_PRIMARY_ATA_CONTROL          0x3f6
 
 #define SECONDARY_ATA_IRQ                 15
 #define PORT_SECONDARY_ATA_COMMAND_BASE   0x170
 #define PORT_SECONDARY_ATA_COMMAND_COUNT  8
+#define PORT_SECONDARY_ATA_COMMAND_LAST   (PORT_SECONDARY_ATA_COMMAND_BASE + PORT_SECONDARY_ATA_COMMAND_COUNT - 1)
 #define PORT_SECONDARY_ATA_CONTROL        0x376
 
+// Command port registers
+enum Register : uint8_t {
+    RegData = 0,           // Read/write
+    RegError = 1,          // Read-only
+    RegFeatures = 1,       // Write-only
+    RegSectorCount = 2,    // Read/write
+    RegSectorNumber = 3,   // Read/write
+    RegCylinderLow = 4,    // Read/write
+    RegCylinderHigh = 5,   // Read/write
+    RegDeviceHead = 6,     // Read/write
+    RegStatus = 7,         // Read-only
+    RegCommand = 7,        // Write-only
+};
+
+enum Channel : uint8_t {
+    ChanPrimary = 0,
+    ChanSecondary = 1,
+};
+
+// Control port has two registers:
+//   Alternate Status when reading
+//   Device Control when writing
 
 class ATA : public IODevice {
 public:
@@ -28,8 +58,32 @@ public:
 
     bool IORead(uint32_t port, uint32_t *value, uint8_t size) override;
     bool IOWrite(uint32_t port, uint32_t value, uint8_t size) override;
+
 private:
     i8259 *m_pic;
+  
+    bool ReadCommandPort(Register reg, Channel channel, uint32_t *value, uint8_t size);
+    bool WriteCommandPort(Register reg, Channel channel, uint32_t value, uint8_t size);
+
+    bool ReadControlPort(Channel channel, uint32_t *value, uint8_t size);
+    bool WriteControlPort(Channel channel, uint32_t value, uint8_t size);
+
+    // Command operations
+    bool ReadData(Channel channel, uint32_t *value, uint8_t size);
+    bool ReadError(Channel channel, uint32_t *value, uint8_t size);
+    bool ReadStatus(Channel channel, uint32_t *value, uint8_t size);
+
+    bool WriteData(Channel channel, uint32_t value, uint8_t size);
+    bool WriteCommand(Channel channel, uint32_t value, uint8_t size);
+
+    // Registers
+    uint8_t m_reg_features;
+    uint8_t m_reg_sectorCount;
+    uint8_t m_reg_sectorNumber;
+    uint16_t m_reg_cylinder;
+    uint8_t m_reg_deviceHead;
 };
 
+}
+}
 }
