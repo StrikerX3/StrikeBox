1d4c76909fce776d078fc516216f0707824cdd8a
StrikerX3 <ivan.rober@gmail.com>
2019-12-09 00:04:26 -0300

    Display actual address of unaligned reads/writes in NV2A engines

--- modules/core/src/common/strikebox/hw/gpu/engine.cpp
+++ modules/core/src/common/strikebox/hw/gpu/engine.cpp
@@ -15,7 +15,7 @@ namespace strikebox::nv2a {
 uint32_t NV2AEngine::ReadUnaligned(const uint32_t addr, const uint8_t size) {
     // Unaligned read fits in one register
     if ((addr & ~3) == ((addr + size - 1) & ~3)) {
-        log_warning("NV2AEngine::Read:   Unaligned read!   address = 0x%x,  size = %u\n", addr, size);
+        log_warning("NV2AEngine::Read:   Unaligned read!   address = 0x%x,  size = %u\n", addr + m_offset, size);
         uint8_t offset = (addr & 3) * 8;
         uint32_t mask = 0xFFFFFFFF >> (32 - size * 8);
         return (Read(addr & ~3) >> offset) & mask;
@@ -25,7 +25,7 @@ uint32_t NV2AEngine::ReadUnaligned(const uint32_t addr, const uint8_t size) {
     // - 16-bit read from offset 3
     // - 32-bit read from offsets 1, 2 or 3
     // FIXME: how does the real hardware behave in this case?
-    log_warning("NV2AEngine::Read:   Severely unaligned read!   address = 0x%x,  size = %u\n", addr, size);
+    log_warning("NV2AEngine::Read:   Severely unaligned read!   address = 0x%x,  size = %u\n", addr + m_offset, size);
     
     uint32_t value1 = Read(addr & ~3);
     uint32_t value2 = Read((addr & ~3) + 4);
@@ -36,7 +36,7 @@ uint32_t NV2AEngine::ReadUnaligned(const uint32_t addr, const uint8_t size) {
 }
 
 void NV2AEngine::WriteUnaligned(const uint32_t addr, const uint32_t value, const uint8_t size) {
-    log_warning("NV2AEngine::Write:  Unaligned write!   address = 0x%x,  size = %u\n", addr, size);
+    log_warning("NV2AEngine::Write:  Unaligned write!   address = 0x%x,  size = %u\n", addr + m_offset, size);
     // FIXME: how does the real hardware behave in this case?
     // This is almost certainly wrong.
     Write(addr & ~3, value);
