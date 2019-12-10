4ac028f2527941c926b31c2af6e6d106c43274b9
StrikerX3 <ivan.rober@gmail.com>
2019-12-09 23:09:50 -0300

    Describe RAMHT and RAMFC registers

--- modules/core/include/strikebox/hw/gpu/engines/pfifo.h
+++ modules/core/include/strikebox/hw/gpu/engines/pfifo.h
@@ -33,9 +33,41 @@ const uint32_t Reg_PFIFO_INTR_ENABLE = 0x140;                  // [RW] Interrupt
 /**/const uint32_t Val_PFIFO_INTR_DMA_PT = (1 << 16);          //  bit 16: DMA PT
 /**/const uint32_t Val_PFIFO_INTR_SEMAPHORE = (1 << 20);       //  bit 20: Semaphore
 /**/const uint32_t Val_PFIFO_INTR_ACQUIRE_TIMEOUT = (1 << 24); //  bit 24: Acquire timeout
-const uint32_t Reg_PFIFO_RAMHT = 0x210;                        // [RW] RAMHT (hash table) address
-const uint32_t Reg_PFIFO_RAMFC = 0x214;                        // [RW] RAMFC (FIFO context) address
+const uint32_t Reg_PFIFO_RAMHT = 0x210;                        // [RW] RAMHT (hash table) parameters
+const uint32_t Reg_PFIFO_RAMFC = 0x214;                        // [RW] RAMFC (FIFO context) parameters
 
+// --- RAMHT parameters ------------
+
+union RAMHTParameters {
+    enum class Size : uint32_t { _4K, _8K, _16K, _32K };
+    enum class Search : uint32_t { _16, _32, _64, _128 };
+
+    uint32_t u32;
+    struct {
+        uint32_t _unused1 : 4;     //  3.. 0 = unused
+        uint32_t baseAddress : 5;  //  8.. 5 = base address of hash table
+        uint32_t _unused2 : 7;     // 15.. 9 = unused
+        Size size : 2;             // 17..16 = size of hash table in bytes
+        uint32_t _unused3 : 6;     // 23..18 = unused
+        Search search : 2;         // 25..24 = entry search stride in bytes
+    };
+};
+static_assert(sizeof(RAMHTParameters) == sizeof(uint32_t));
+
+// --- RAMFC parameters ------------
+
+union RAMFCParameters {
+    enum class Size : uint32_t { _1K, _2K };
+    uint32_t u32;
+    struct {
+        uint32_t _unused1 : 2;      //  1.. 0 = unused
+        uint32_t baseAddress1 : 7;  //  8.. 2 = base address of FIFO context 1
+        uint32_t _unused2 : 7;      // 15.. 9 = unused
+        Size size : 1;              // 16..16 = size of FIFO context in bytes
+        uint32_t baseAddress2 : 7;  // 23..17 = base address of FIFO context 2
+    };
+};
+static_assert(sizeof(RAMFCParameters) == sizeof(uint32_t));
 
 // ----------------------------------------------------------------------------
 
@@ -58,8 +90,8 @@ private:
     uint32_t m_interruptLevels;
     uint32_t m_enabledInterrupts;
     
-    uint32_t m_ramhtParams;
-    uint32_t m_ramfcParams;
+    RAMHTParameters m_ramhtParams;
+    RAMFCParameters m_ramfcParams;
 };
 
 }
--- modules/core/src/common/strikebox/hw/gpu/engines/pfifo.cpp
+++ modules/core/src/common/strikebox/hw/gpu/engines/pfifo.cpp
@@ -14,6 +14,38 @@
 
 namespace strikebox::nv2a {
 
+static inline void printRAMHTParameters(RAMHTParameters& params) {
+#if LOG_LEVEL >= LOG_LEVEL_SPEW
+    log_spew("[NV2A] PFIFO RAMHT updated:  base addr = 0x%x,  size = ", params.baseAddress);
+    switch (params.size) {
+    case RAMHTParameters::Size::_4K: log_spew("4K"); break;
+    case RAMHTParameters::Size::_8K: log_spew("8K"); break;
+    case RAMHTParameters::Size::_16K: log_spew("16K"); break;
+    case RAMHTParameters::Size::_32K: log_spew("32K"); break;
+    }
+    log_spew(",  search = ");
+    switch (params.search) {
+    case RAMHTParameters::Search::_16: log_spew("16"); break;
+    case RAMHTParameters::Search::_32: log_spew("32"); break;
+    case RAMHTParameters::Search::_64: log_spew("64"); break;
+    case RAMHTParameters::Search::_128: log_spew("128"); break;
+    }
+    log_spew("\n");
+#endif
+}
+
+static inline void printRAMFCParameters(RAMFCParameters& params) {
+#if LOG_LEVEL >= LOG_LEVEL_SPEW
+    log_spew("[NV2A] PFIFO RAMFC updated:  base addr 1 = 0x%x,  base addr 2 = 0x%x,  size = ", params.baseAddress1, params.baseAddress2);
+    switch (params.size) {
+    case RAMFCParameters::Size::_1K: log_spew("1K\n"); break;
+    case RAMFCParameters::Size::_2K: log_spew("2K\n"); break;
+    }
+#endif
+}
+
+// ----------------------------------------------------------------------------
+
 void PFIFO::SetEnabled(bool enabled) {
     if (m_enabled != enabled) {
         m_enabled = enabled;
@@ -30,8 +62,8 @@ void PFIFO::Reset() {
     m_enabled = false;
     m_interruptLevels = 0;
     m_enabledInterrupts = 0;
-    m_ramhtParams = 0;
-    m_ramfcParams = 0;
+    m_ramhtParams.u32 = 0;
+    m_ramfcParams.u32 = 0;
     // TODO: stop threads
 }
 
@@ -39,8 +71,8 @@ uint32_t PFIFO::Read(const uint32_t addr) {
     switch (addr) {
     case Reg_PFIFO_INTR: return m_interruptLevels;
     case Reg_PFIFO_INTR_ENABLE: return m_enabledInterrupts;
-    case Reg_PFIFO_RAMHT: return m_ramhtParams;
-    case Reg_PFIFO_RAMFC: return m_ramfcParams;
+    case Reg_PFIFO_RAMHT: return m_ramhtParams.u32;
+    case Reg_PFIFO_RAMFC: return m_ramfcParams.u32;
     default:
         log_spew("[NV2A] PFIFO::Read:   Unimplemented read!   address = 0x%x\n", addr);
         return 0;
@@ -59,10 +91,12 @@ void PFIFO::Write(const uint32_t addr, const uint32_t value) {
         m_nv2a.UpdateIRQ();
         break;
     case Reg_PFIFO_RAMHT:
-        m_ramhtParams = value;
+        m_ramhtParams.u32 = value;
+        printRAMHTParameters(m_ramhtParams);
         break;
     case Reg_PFIFO_RAMFC:
-        m_ramfcParams = value;
+        m_ramfcParams.u32 = value;
+        printRAMFCParameters(m_ramfcParams);
         break;
     default:
         log_spew("[NV2A] PFIFO::Write:  Unimplemented write!   address = 0x%x,  value = 0x%x\n", addr, value);
