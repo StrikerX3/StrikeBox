1173e59e6a5f94e3191f6c1b89256aa158f9b926
StrikerX3 <ivan.rober@gmail.com>
2019-12-14 12:52:25 -0300

    Figure out most PGRAPH registers used during startup

--- modules/core/include/strikebox/hw/gpu/engines/pgraph.h
+++ modules/core/include/strikebox/hw/gpu/engines/pgraph.h
@@ -20,8 +20,94 @@ namespace strikebox::nv2a {
 // PGRAPH registers
 // [https://envytools.readthedocs.io/en/latest/hw/graph/kelvin/pgraph.html#mmio-registers]
 // [https://github.com/torvalds/linux/blob/master/drivers/gpu/drm/nouveau/nvkm/engine/gr/regs.h]
-const uint32_t Reg_PGRAPH_INTR = 0x100;        // [RW] Interrupt status
-const uint32_t Reg_PGRAPH_INTR_ENABLE = 0x140; // [RW] Interrupt enable
+const uint32_t Reg_PGRAPH_INTR = 0x100;                 // [RW] Interrupt status
+const uint32_t Reg_PGRAPH_INTR_ENABLE = 0x140;          // [RW] Interrupt enable
+/**/const uint32_t Reg_PGRAPH_INTR_NOTIFY = (1 << 0);               // bit  0: notify
+/**/const uint32_t Reg_PGRAPH_INTR_MISSING_HW = (1 << 4);           // bit  4: missing hardware
+/**/const uint32_t Reg_PGRAPH_INTR_TLB_PRESENT_DMA_R = (1 << 6);    // bit  6: DMA read TLB not present or invalid
+/**/const uint32_t Reg_PGRAPH_INTR_TLB_PRESENT_DMA_W = (1 << 7);    // bit  7: DMA write TLB not present or invalid
+/**/const uint32_t Reg_PGRAPH_INTR_TLB_PRESENT_TEX_A = (1 << 8);    // bit  8: texture A TLB not present or invalid
+/**/const uint32_t Reg_PGRAPH_INTR_TLB_PRESENT_TEX_B = (1 << 9);    // bit  9: texture B TLB not present or invalid
+/**/const uint32_t Reg_PGRAPH_INTR_TLB_PRESENT_VTX = (1 << 10);     // bit 10: vertex TLB not present or invalid
+/**/const uint32_t Reg_PGRAPH_INTR_CONTEXT_SWITCH = (1 << 12);      // bit 12: context switch
+/**/const uint32_t Reg_PGRAPH_INTR_STATE3D = (1 << 13);             // bit 13: 3D state
+/**/const uint32_t Reg_PGRAPH_INTR_BUFFER_NOTIFY = (1 << 16);       // bit 16: buffer notify
+/**/const uint32_t Reg_PGRAPH_INTR_ERROR = (1 << 20);               // bit 20: error
+/**/const uint32_t Reg_PGRAPH_INTR_SINGLE_STEP = (1 << 24);         // bit 24: single step complete
+
+const uint32_t Reg_PGRAPH_CTX_CONTROL = 0x144;          // [RW] Context control (PGRAPHContextControl)
+const uint32_t Reg_PGRAPH_CTX_SWITCH1 = 0x14c;          // [RW] Context switch 1
+const uint32_t Reg_PGRAPH_CTX_SWITCH2 = 0x150;          // [RW] Context switch 2
+const uint32_t Reg_PGRAPH_CTX_SWITCH3 = 0x154;          // [RW] Context switch 3
+const uint32_t Reg_PGRAPH_CTX_SWITCH4 = 0x158;          // [RW] Context switch 4
+const uint32_t Reg_PGRAPH_CTX_SWITCH5 = 0x15c;          // [RW] Context switch 5
+const uint32_t Reg_PGRAPH_STATUS = 0x700;               // [R ] PGRAPH status
+const uint32_t Reg_PGRAPH_FIFO = 0x720;                 // [RW] FIFO state (bit 0: 0 = disabled, 1 = enabled)
+const uint32_t Reg_PGRAPH_RDI_INDEX = 0x750;            // [RW] RDI index (RDIIndex)
+const uint32_t Reg_PGRAPH_RDI_DATA = 0x754;             // [RW] RDI data
+const uint32_t Reg_PGRAPH_FFINTFC_ST2 = 0x764;          // [RW] ??
+const uint32_t Reg_PGRAPH_CHANNEL_CTX_TABLE = 0x780;    // [RW] Channel context table (low 16 bits only)
+const uint32_t Reg_PGRAPH_CHANNEL_CTX_POINTER = 0x784;  // [RW] Channel context pointer (low 16 bits only)
+
+const uint32_t Reg_PGRAPH_TILE_BASE = 0x900;            //      Base address of tiles
+const uint32_t Reg_PGRAPH_TILE = 0x0;                   // [RW]   Tile address
+const uint32_t Reg_PGRAPH_TLIMIT = 0x4;                 // [RW]   Tile limit
+const uint32_t Reg_PGRAPH_TSIZE = 0x8;                  // [RW]   Tile size (pitch)
+const uint32_t Reg_PGRAPH_TSTATUS = 0xc;                // [RW]   Tile status
+const size_t kPGRAPH_NumTiles = 8;
+
+const uint32_t Reg_PGRAPH_ZCOMP_BASE = 0x980;           //      Base address of ZCOMP (?)
+const size_t kPGRAPH_NumZCOMP = 8;
+
+// ----------------------------------------------------------------------------
+
+// --- RDI -------------------------
+
+union RDIIndex {
+    uint32_t u32;
+    struct {
+        uint32_t
+            : 2,           //  1.. 0 = unused
+            address : 11,  // 12.. 2 = address
+            : 3,           // 15..13 = unused
+            select : 9;    // 24..16 = select
+    };
+};
+static_assert(sizeof(RDIIndex) == sizeof(uint32_t));
+
+// --- PGRAPH context control ------
+
+union PGRAPHContextControl {
+    uint32_t u32;
+    struct {
+        uint32_t
+            minimumTime : 2,    //  1.. 0 = minimum time (?)
+            : 6,                //  7.. 2 = unused
+            time : 1,           //  8.. 8 = time (?)
+            : 7,                // 15.. 9 = unused
+            channelValid : 1,   // 16..16 = channel is valid? (1 = yes)
+            : 3,                // 19..17 = unused
+            change : 1,         // 20..20 = change (?)
+            : 3,                // 23..21 = unused
+            switching : 1,      // 24..24 = switching (?)
+            : 3,                // 27..25 = unused
+            device : 1;         // 28..28 = device (?)
+    };
+};
+static_assert(sizeof(PGRAPHContextControl) == sizeof(uint32_t));
+
+// --- PGRAPH tile -----------------
+
+union PGRAPHTile {
+    uint32_t u32[4];
+    struct {
+        uint32_t address;
+        uint32_t limit;
+        uint32_t pitch;
+        uint32_t status;
+    };
+};
+static_assert(sizeof(PGRAPHTile) == sizeof(uint32_t[4]));
 
 // ----------------------------------------------------------------------------
 
@@ -43,6 +129,24 @@ private:
 
     uint32_t m_interruptLevels;
     uint32_t m_enabledInterrupts;
+
+    PGRAPHContextControl m_ctxControl;
+    uint32_t m_ctxSwitches[5];
+    bool m_fifoEnabled;
+    uint32_t m_status;
+
+    uint32_t m_rdiIndex;
+
+    uint32_t m_ffintfc_st2;
+
+    uint16_t m_channelCtxTable;
+    uint16_t m_channelCtxPointer;
+
+    PGRAPHTile m_tiles[kPGRAPH_NumTiles];
+    uint32_t m_zcomp[kPGRAPH_NumZCOMP];
+
+    uint32_t RDIRead();
+    uint32_t RDIWrite(const uint32_t value);
 };
 
 }
--- modules/core/src/common/strikebox/hw/gpu/engines/pgraph.cpp
+++ modules/core/src/common/strikebox/hw/gpu/engines/pgraph.cpp
@@ -28,16 +28,60 @@ void PGRAPH::SetEnabled(bool enabled) {
 
 void PGRAPH::Reset() {
     m_enabled = false;
+    // TODO: stop threads
+ 
     m_interruptLevels = 0;
     m_enabledInterrupts = 0;
-    // TODO: stop threads
+
+    m_ctxControl.u32 = 0;
+    std::fill(std::begin(m_ctxSwitches), std::end(m_ctxSwitches), 0);
+    m_fifoEnabled = false;
+    m_status = 0;
+
+    m_rdiIndex = 0;
+
+    m_ffintfc_st2 = 0;
+
+    m_channelCtxTable = 0;
+    m_channelCtxPointer = 0;
+
+    std::fill(std::begin(m_tiles), std::end(m_tiles), PGRAPHTile{ 0 });
+    std::fill(std::begin(m_zcomp), std::end(m_zcomp), 0);
 }
 
 uint32_t PGRAPH::Read(const uint32_t addr) {
     switch (addr) {
     case Reg_PGRAPH_INTR: return m_interruptLevels;
     case Reg_PGRAPH_INTR_ENABLE: return m_enabledInterrupts;
+
+    case Reg_PGRAPH_CTX_CONTROL: return m_ctxControl.u32;
+    case Reg_PGRAPH_CTX_SWITCH1: return m_ctxSwitches[0];
+    case Reg_PGRAPH_CTX_SWITCH2: return m_ctxSwitches[1];
+    case Reg_PGRAPH_CTX_SWITCH3: return m_ctxSwitches[2];
+    case Reg_PGRAPH_CTX_SWITCH4: return m_ctxSwitches[3];
+    case Reg_PGRAPH_CTX_SWITCH5: return m_ctxSwitches[4];
+    case Reg_PGRAPH_STATUS: return m_status;
+    case Reg_PGRAPH_FIFO: return m_fifoEnabled ? 1 : 0;
+    case Reg_PGRAPH_RDI_INDEX: return m_rdiIndex;
+    case Reg_PGRAPH_RDI_DATA: return RDIRead();
+    case Reg_PGRAPH_FFINTFC_ST2: return m_ffintfc_st2;
+    case Reg_PGRAPH_CHANNEL_CTX_TABLE: return m_channelCtxTable;
+    case Reg_PGRAPH_CHANNEL_CTX_POINTER: return m_channelCtxPointer;
+
     default:
+        if (addr >= Reg_PGRAPH_TILE_BASE && addr < Reg_PGRAPH_TILE_BASE + kPGRAPH_NumTiles * sizeof(PGRAPHTile)) {
+            uint32_t tileIndex = ((addr - Reg_PGRAPH_TILE_BASE) >> 4) & 7;
+            switch (addr & 0xF) {
+            case Reg_PGRAPH_TILE: return m_tiles[tileIndex].address;
+            case Reg_PGRAPH_TLIMIT: return m_tiles[tileIndex].limit;
+            case Reg_PGRAPH_TSIZE: return m_tiles[tileIndex].pitch;
+            case Reg_PGRAPH_TSTATUS: return m_tiles[tileIndex].status;
+            }
+        }
+        else if (addr >= Reg_PGRAPH_ZCOMP_BASE && addr < Reg_PGRAPH_ZCOMP_BASE + kPGRAPH_NumZCOMP * sizeof(uint32_t)) {
+            uint32_t zcompIndex = ((addr - Reg_PGRAPH_ZCOMP_BASE) >> 2) & (kPGRAPH_NumZCOMP - 1);
+            return m_zcomp[zcompIndex];
+        }
         log_spew("[NV2A] PGRAPH::Read:   Unimplemented read!   address = 0x%x\n", addr);
         return 0;
     }
@@ -45,6 +89,10 @@ uint32_t PGRAPH::Read(const uint32_t addr) {
 
 void PGRAPH::Write(const uint32_t addr, const uint32_t value) {
     switch (addr) {
+    case Reg_PGRAPH_STATUS:
+        // Discard writes to read-only registers
+        break;
+
     case Reg_PGRAPH_INTR:
         // Clear specified interrupts
         m_interruptLevels &= ~value;
@@ -54,9 +102,56 @@ void PGRAPH::Write(const uint32_t addr, const uint32_t value) {
         m_enabledInterrupts = value;
         m_nv2a.UpdateIRQ();
         break;
+
+    case Reg_PGRAPH_CTX_CONTROL: m_ctxControl.u32 = value; break;
+    case Reg_PGRAPH_CTX_SWITCH1: m_ctxSwitches[0] = value; break;
+    case Reg_PGRAPH_CTX_SWITCH2: m_ctxSwitches[1] = value; break;
+    case Reg_PGRAPH_CTX_SWITCH3: m_ctxSwitches[2] = value; break;
+    case Reg_PGRAPH_CTX_SWITCH4: m_ctxSwitches[3] = value; break;
+    case Reg_PGRAPH_CTX_SWITCH5: m_ctxSwitches[4] = value; break;
+    case Reg_PGRAPH_FIFO: m_fifoEnabled = value & 1; break;
+    case Reg_PGRAPH_RDI_INDEX: m_rdiIndex = value; break;
+    case Reg_PGRAPH_RDI_DATA: RDIWrite(value); break;
+    case Reg_PGRAPH_FFINTFC_ST2: m_ffintfc_st2 = value; break;
+    case Reg_PGRAPH_CHANNEL_CTX_TABLE: m_channelCtxTable = value; break;
+    case Reg_PGRAPH_CHANNEL_CTX_POINTER: m_channelCtxPointer = value; break;
+
     default:
-        log_spew("[NV2A] PGRAPH::Write:  Unimplemented write!   address = 0x%x,  value = 0x%x\n", addr, value);
+        if (addr >= Reg_PGRAPH_TILE_BASE && addr < Reg_PGRAPH_TILE_BASE + kPGRAPH_NumTiles * sizeof(PGRAPHTile)) {
+            uint32_t tileIndex = ((addr - Reg_PGRAPH_TILE_BASE) >> 4) & 7;
+            switch (addr & 0xF) {
+            case Reg_PGRAPH_TILE: m_tiles[tileIndex].address = value; break;
+            case Reg_PGRAPH_TLIMIT: m_tiles[tileIndex].limit = value; break;
+            case Reg_PGRAPH_TSIZE: m_tiles[tileIndex].pitch = value; break;
+            case Reg_PGRAPH_TSTATUS: m_tiles[tileIndex].status = value; break;
+            }
+        }
+        else if (addr >= Reg_PGRAPH_ZCOMP_BASE && addr < Reg_PGRAPH_ZCOMP_BASE + kPGRAPH_NumZCOMP * sizeof(uint32_t)) {
+            uint32_t zcompIndex = ((addr - Reg_PGRAPH_ZCOMP_BASE) >> 2) & (kPGRAPH_NumZCOMP - 1);
+            m_zcomp[zcompIndex] = value;
+        }
+        else {
+            log_spew("[NV2A] PGRAPH::Write:  Unimplemented write!   address = 0x%x,  value = 0x%x\n", addr, value);
+        }
         break;
+    }
+}
+
+uint32_t PGRAPH::RDIRead() {
+    // TODO: implement known RDI registers
+    switch (m_rdiIndex) {
+    default:
+        log_spew("[NV2A] PGRAPH::RDIRead:   Unimplemented RDI read!   index = 0x%x\n", m_rdiIndex);
+        return 0;
+    }
+}
+
+uint32_t PGRAPH::RDIWrite(const uint32_t value) {
+    // TODO: implement known RDI registers
+    switch (m_rdiIndex) {
+    default:
+        log_spew("[NV2A] PGRAPH::RDIWrite:  Unimplemented RDI write!   index = 0x%x,  value = 0x%x\n", m_rdiIndex, value);
+        return 0;
     }
 }
 
